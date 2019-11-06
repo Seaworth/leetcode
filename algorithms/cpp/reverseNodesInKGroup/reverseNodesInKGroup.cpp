1edeb9a22c8c54c8477b6bef072bd23b0f5b018e
Seaworth <baihcuanemail@qq.com>
2019-11-06 09:28:16 +0800

    modify 25

--- algorithms/cpp/reverseNodesInKGroup/reverseNodesInKGroup.cpp
+++ algorithms/cpp/reverseNodesInKGroup/reverseNodesInKGroup.cpp
@@ -1,6 +1,6 @@
 // Source : https://oj.leetcode.com/problems/reverse-nodes-in-k-group/
-// Author : Hao Chen
-// Date   : 2014-07-05
+// Author : Seaworth
+// Date   : 2019-11-06
 
 /********************************************************************************** 
 * 
@@ -22,54 +22,33 @@
 *               
 **********************************************************************************/
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <iostream>
-using namespace std;
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
-ListNode *reverseList(ListNode *&head, int k);
-
-ListNode *reverseKGroup(ListNode *head, int k) {
-
-    if (k<=0) return head;
-    ListNode fake(0);
-    fake.next = head;
-    ListNode* p = &fake;
-
-    while(p){
-        p->next = reverseList(p->next, k);
-        for(int i=0; p && i<k; i++){
-            p = p->next;
+class Solution {
+public:
+    ListNode *reverseKGroup(ListNode *head, int k) {
+        if(head==NULL||k==1) return head;
+        int num=0;
+        ListNode *preheader = new ListNode(-1);
+        preheader->next = head;
+        ListNode *cur = preheader, *nex, *pre = preheader;
+        while(cur = cur->next) 
+            num++;
+        while(num>=k) {
+            cur = pre->next;
+            nex = cur->next;
+            for(int i=1;i<k;++i) {
+                cur->next=nex->next;
+                nex->next=pre->next;
+                pre->next=nex;
+                nex=cur->next;
+            }
+            pre = cur;
+            num-=k;
         }
+        return preheader->next;
     }
-
-    return fake.next;
-}
-
-ListNode *reverseList(ListNode *&head, int k){
-    ListNode* pEnd=head;
-    while (pEnd && k>0){
-        pEnd = pEnd->next;
-        k--;
-    }
-    if (k>0) return head;
-
-    ListNode *pHead=pEnd, *p=head;
-    while(p != pEnd){
-        ListNode *q = p->next;
-        p->next = pHead;
-        pHead = p;
-        p = q;
-    }
-    return pHead;
-}
-
+};
+//打印链表
 void printList(ListNode* h)
 {
     while(h!=NULL){
@@ -77,35 +56,4 @@ void printList(ListNode* h)
         h = h->next;
     }
     printf("\n");
-}
-
-ListNode* createList(int a[], int n)
-{
-    ListNode *head=NULL, *p=NULL;
-    for(int i=0; i<n; i++){
-        if (head == NULL){
-            head = p = new ListNode(a[i]);
-        }else{
-            p->next = new ListNode(a[i]);
-            p = p->next;
-        }
-    }
-    return head;
-}
-
-
-int main(int argc, char** argv)
-{
-    int a[] = {1,2,3,4,5,6,7,8,9,10};
-    ListNode* pList = createList(a, sizeof(a)/sizeof(int));
-
-    int k =2;
-    if (argc>1){
-        k = atoi(argv[1]);
-    }
-
-    pList = reverseKGroup(pList, k);
-    printList(pList);
-    
-    return 0;
 }
